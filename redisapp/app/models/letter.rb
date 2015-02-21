066e994605a625db41053f51379fb72e21099137
Toth Peter <tothpeter08@gmail.com>
2015-02-21 22:22:51 +0100

    2.3-Top Listings (storing ids in redis)

--- redisapp/app/models/letter.rb
+++ redisapp/app/models/letter.rb
@@ -1,5 +1,8 @@
+require "letter_redis_repository"
+
 class Letter < ActiveRecord::Base
   def self.top_10
-    limit(10).order(score: :desc)
+    ids = LetterRedisRepository.top
+    where id: ids
   end
 end

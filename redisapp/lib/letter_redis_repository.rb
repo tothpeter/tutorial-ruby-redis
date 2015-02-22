df62eb42cc632b7d38088ff2da8468608239c936
Toth Peter <tothpeter08@gmail.com>
2015-02-22 15:50:10 +0100

    Refactor adding letters to the top list

--- redisapp/lib/letter_redis_repository.rb
+++ redisapp/lib/letter_redis_repository.rb
@@ -3,9 +3,11 @@ class LetterRedisRepository
     list = redis.zrevrange "top_letters", 0, 9
     if list.empty?
       letters = Letter.limit(10).order(score: :desc)
-      letters.each do |letter|
-        redis.zadd "top_letters", letter.score, letter.id
-      end
+      redis.zadd "top_letters", letters.map { |letter| [letter.score, letter.id] }
+      
+      # letters.each do |letter|
+      #   redis.zadd "top_letters", letter.score, letter.id
+      # end
 
       return letters.map(&:id)
     else
